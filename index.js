040993e98b5a547ffbf8af194aa7a260a6320ece
Krina <krina2883@gmail.com>
2024-12-19 20:13:29 +0530

    movie web using node

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const app = express();
+
+const port = 2612;
+
+app.set('view engine', 'ejs')
+
+app.use(express.urlencoded());
+
+app.use('/', require('./routes/indexRoute'));
+
+const db = require('./config/db');
+
+const path = require('path');
+
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
+
+app.get('/', (req, res) => {
+    return res.render('index');
+})
+
+app.listen(port, () => {
+
+
+    console.log(`Server Start at http://localhost:${port}`);
+})
